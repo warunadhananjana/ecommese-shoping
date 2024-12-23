e5cc4329ddc7b153c94e77e93171401a704c0974
dhananjana123 <warunad999@gmail.com>
2024-12-23 07:59:24 +0530

    controller change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,20 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-use App\Models\User;
-
 class HomeController extends Controller
 {
-    public function redirect(){
+    // Ensure this controller uses authentication middleware
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-        $usertype=Auth::user()->usertype;
+    public function redirect()
+    {
+        $usertype = Auth::user()->usertype;
 
-        if($usertype=='1'){
-
+        // Redirect based on user type
+        if ($usertype === '1') {
             return view('admin.home');
-        }
-        else 
-        {
+        } else {
             return view('dashboard');
         }
     }
